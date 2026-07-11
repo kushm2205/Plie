fa389633548e5713e0cb6fc46736526277505c35
kushm2205 <kushp2224@gmail.com>
2026-07-11 13:17:58 +0530

    unnesscry thing removed

--- plie-redux-toolkit/src/store/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from './index';
-
-// Use these typed hooks everywhere instead of the plain `useDispatch` and
-// `useSelector` so screens get full TypeScript autocompletion/safety.
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
